88a3fc262d5b43c62cb9a1253ba25e0e20eec367
Jacek Pasierb <jack_byk@o2.pl>
2024-12-23 22:05:54 +0100

    Feature/Testimontials - addnew section with opinion and create carousel

--- src/data/opinions.tsx
+++ src/data/opinions.tsx
@@ -1,17 +1,21 @@
+import person1 from "../image/deborah-quagmire-120x120.jpg";
+import person2 from "../image/benedict-arnold-120x120.jpg";
+import person3 from "../image/testimonials-3-120x120.jpg";
+
 export const opinions = [
-    {
-      picture: "../image/deborah-quagmire-120x120.jpg",
-      info: "For 22 years, we have partnered with Edifice on a variety of projects, and each time they are completed on time and on  budget. Our relationship is built on quality and trust. We recommend them for any job—big or small.",
-      author: "Jane Smith",
-    },
-    {
-      picture: "../image/benedict-arnold-120x120.jpg",
-      info: "Our company needed to build a brand new 28,000 sf facility that would both meet our needs and comply with world standards. Edifice alleviated all of our concerns and communicated with us every step of the way.",
-      author: "James Wilson",
-    },
-    {
-      picture: "../image/testimonials-3-120x120.jpg",
-      info: "Our business was expanding and needed additional manufacturing space. We went into this project knowing Edifice was up for the challenge. and shared our goals. The results exceeded our expectations, to say the least.",
-      author: "Ann Williams",
-    },
-  ];
+  {
+    picture: { person1 },
+    info: "For 22 years, we have partnered with Edifice on a variety of projects, and each time they are completed on time and on  budget. Our relationship is built on quality and trust. We recommend them for any job—big or small.",
+    author: "Jane Smith",
+  },
+  {
+    picture: { person2 },
+    info: "Our company needed to build a brand new 28,000 sf facility that would both meet our needs and comply with world standards. Edifice alleviated all of our concerns and communicated with us every step of the way.",
+    author: "James Wilson",
+  },
+  {
+    picture: { person3 },
+    info: "Our business was expanding and needed additional manufacturing space. We went into this project knowing Edifice was up for the challenge. and shared our goals. The results exceeded our expectations, to say the least.",
+    author: "Ann Williams",
+  },
+];
