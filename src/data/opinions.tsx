af6b8d6b8bcc72d91837a3d18786e777bcf5adca
Jacek Pasierb <jack_byk@o2.pl>
2024-12-23 22:02:53 +0100

    Feature/Testimontials - addnew section with opinion and create carousel

--- src/data/opinions.tsx
+++ src/data/opinions.tsx
@@ -1,16 +1,16 @@
 export const opinions = [
     {
-      picture: "src/image/deborah-quagmire-120x120.jpg",
+      picture: "../image/deborah-quagmire-120x120.jpg",
       info: "For 22 years, we have partnered with Edifice on a variety of projects, and each time they are completed on time and on  budget. Our relationship is built on quality and trust. We recommend them for any job—big or small.",
       author: "Jane Smith",
     },
     {
-      picture: "src/image/benedict-arnold-120x120.jpg",
+      picture: "../image/benedict-arnold-120x120.jpg",
       info: "Our company needed to build a brand new 28,000 sf facility that would both meet our needs and comply with world standards. Edifice alleviated all of our concerns and communicated with us every step of the way.",
       author: "James Wilson",
     },
     {
-      picture: "src/image/testimonials-3-120x120.jpg",
+      picture: "../image/testimonials-3-120x120.jpg",
       info: "Our business was expanding and needed additional manufacturing space. We went into this project knowing Edifice was up for the challenge. and shared our goals. The results exceeded our expectations, to say the least.",
       author: "Ann Williams",
     },
